01b89d3bb8401598dc201f13a64b86b8b69fb133
Azat Yamalov <yamalov.azat@live.com>
2022-03-05 11:39:23 +0300

    pl-icon-button variants and props(hidden, negative)

--- pl-icon-button.js
+++ pl-icon-button.js
@@ -2,17 +2,21 @@ import { PlElement, html, css } from "polylib";
 import "@plcmp/pl-icon";
 
 class PlIconButton extends PlElement {
-	static get properties() {
-		return {
-			iconset: { type: String },
-            icon: { type: String},
-			disabled: { type: Boolean, reflect: true },
-            tabindex: { type: String, value: '0', reflectToAttribute: true}
-		}
-	}
+    static get properties() {
+        return {
+            iconset: { type: String },
+            size: { type: Number, value: '16' },
+            variant: { type: String, reflectToAttribute: true, value: 'secondary' },
+            icon: { type: String },
+            disabled: { type: Boolean, reflectToAttribute: true },
+            tabindex: { type: String, value: '0', reflectToAttribute: true },
+            hidden: { type: Boolean, reflectToAttribute: true },
+            negative: { type: Boolean, reflectToAttribute: true }
+        }
+    }
 
-	static get css() {
-		return css`
+    static get css() {
+        return css`
 			:host {
                 display: flex;
                 align-items: center;
@@ -21,35 +25,102 @@ class PlIconButton extends PlElement {
                 height: 32px;
                 box-sizing: border-box;
                 border-radius: 4px;
-                color: var(--grey-dark);
+                color: #FFFFFF;
                 user-select: none;
                 cursor: pointer;
                 outline: none;
                 transition: background .3s ease-in-out;
             }
 
-            :host(:hover),:host(:focus) {
-                background: var(--grey-light);
-                color: var(--black-base);
+            /* primary */
+            :host([variant=primary]) {
+                background: var(--primary-base);
             }
 
-            :host(:active) {
-                background: var(--grey-base);
-                color: var(--black-base);
+            :host([variant=primary]:hover),
+            :host([variant=primary]:focus) {
+                background: var(--primary-dark);
+            }
+
+            :host([variant=primary]:active) {
+                background: var(--primary-darkest);
+            }
+
+            /* negative */
+            :host([negative]) {
+                --primary-base: var(--negative-base);
+                --primary-light: var(--negative-light);
+                --primary-lightest: var(--negative-lightest);
+                --primary-dark: var(--negative-dark);
+                --primary-darkest: var(--negative-darkest);
+            }
+        
+            /* secondary */
+            :host([variant=secondary]) {
+                background: var(--primary-lightest);
+                color: var(--primary-base);
+            }
+
+            :host([variant=secondary]:hover),:host([variant=secondary]:focus) {
+                background: var(--primary-light);
+                color: var(--primary-dark);
+            }
+
+            :host([variant=secondary]:active) {
+                background: var(--primary-base);
+                color: var(--primary-darkest);
+            }
+
+            /* ghost */
+            :host([variant=ghost]) {
+                background: transparent;
+                color: var(--primary-base);
+                border: 1px solid var(--primary-base);
+            }
+
+            :host([variant=ghost]:hover),:host([variant=ghost]:focus) {
+                border: 1px solid var(--primary-dark);
+                color: var(--primary-dark);
+                background: var(--primary-lightest);
+            }
+
+            :host([variant=ghost]:active) {
+                background: var(--primary-light);
+                color: var(--primary-darkest);
+                border: 1px solid var(--primary-darkest);
+            }
+
+
+            /* link */
+            :host([variant=link]) {
+                background: transparent;
+                color: var(--primary-base);
+            }
+
+            :host([variant=link]:hover),:host([variant=link]:focus) {
+                background: var(--primary-lightest);
+                color: var(--primary-dark);
+            }
+
+            :host([variant=link]:active) {
+                background: var(--primary-light);
+                color:  var(--primary-darkest);
             }
 
             :host([disabled]) {
-                color: var(--grey-base);
+                background: var(--grey-light);
+                border: none;
+                color: var(--grey-dark);
                 cursor: not-allowed;
                 pointer-events: none;
             }
 		`;
-	}
-	static get template() {
-		return html`
-            <pl-icon iconset="[[iconset]]" icon="[[icon]]"></pl-icon>
+    }
+    static get template() {
+        return html`
+            <pl-icon size="[[size]]" iconset="[[iconset]]" icon="[[icon]]"></pl-icon>
         `;
-	}
+    }
 }
 
 customElements.define('pl-icon-button', PlIconButton);
